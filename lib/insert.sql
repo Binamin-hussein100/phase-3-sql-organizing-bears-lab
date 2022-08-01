eaa812833665942a7f54f0d2ac5bbc0f532b43e3
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-08-01 12:51:45 +0300

    DOne

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Mr.Chocolate',54,'M','brown','high','FALSE');
+
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Rowdy',54,'M','brown','high','FALSE');
+
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Tabitha',54,'F','brown','high','FALSE');
+
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Sergeant Brown',54,'M','brown','high','FALSE');
+
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Melissa',54,'F','brown','high','FALSE');
+
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Grinch',54,'M','brown','high','FALSE');
+
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Wendy',54,'F','brown','high','FALSE');
+
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES (NULL,54,'M','brown','high','FALSE');
